e681c76e7ddf5742f4d332e6d50f0e5daf8127a6
RaoYi <raoyi@users.noreply.github.com>
2019-11-30 03:45:02 +0800

    Update bin2u.sh

--- script/bin2u.sh
+++ script/bin2u.sh
@@ -4,11 +4,11 @@
 # Email: hi@raoyi.net
 # USED FOR CREATE CHROMEOS-INSTALL U-DISK IN LINUX.
 # Usage:
-# run script in Linux Terminal,
+# run script in Linux terminal,
 # use the binary file path as the first parameter.
 
-echo "please choose u-disk volume..."
-var=$(ls /dev/sd[a-p])
+echo "Please choose correct u-disk volume..."
+var=$(ls /dev/sd[a-h])
 OLD_IFS="$IFS"
 IFS=" "
 var=($var)
@@ -21,6 +21,5 @@ done
 read num
 
 pv -preb $1 | sudo dd of=/dev/sd$num bs=8M iflag=fullblock oflag=dsync
-sync
 sync
 echo "finished..."
